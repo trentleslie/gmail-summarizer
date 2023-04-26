a6cb9cb38f95cc2b37b656805a2c155c4b1bedbd
trentleslie <trentleslie@gmail.com>
2023-04-26 21:05:45 +0000

    added full paths for creds so cron would work

--- gmail_summarizer.py
+++ gmail_summarizer.py
@@ -80,16 +80,16 @@ def email_summarizer(text):
 
 def main():
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('/home/ubuntu/gmail-summarizer/token.json'):
+        creds = Credentials.from_authorized_user_file('/home/ubuntu/gmail-summarizer/token.json', SCOPES)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('/home/ubuntu/gmail-summarizer/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
+        with open('/home/ubuntu/gmail-summarizer/token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('gmail', 'v1', credentials=creds)
